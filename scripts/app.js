4727aced8fbaead1d404f10109d7da2c235526d0
Omid Daliri <omiidnk02@gmail.com>
2025-10-06 06:30:18 +0330

    burger menu done

--- scripts/app.js
+++ scripts/app.js
@@ -1,12 +1,16 @@
 // burger menu mobile : remove&add nav__btn--open to html
 const navBtn = document.querySelector(".nav__btn")
+const navMenu = document.querySelector(".mobile-menu")
+
 let navOpen = false;
 navBtn.addEventListener("click",function(){
     if(navOpen){
         navBtn.classList.remove("nav__btn--open")
+        navMenu.classList.remove("mobile-menu--open")
         navOpen = false
     } else {
         navBtn.classList.add("nav__btn--open")
+        navMenu.classList.add("mobile-menu--open")
         navOpen = true
     }
 })
